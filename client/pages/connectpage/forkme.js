ff0368d75dc074d62000f04af448fd56daf25de7
joeshakely <joeshakely@gmail.com>
2024-07-24 12:41:41 -0700

    asdf

--- client/pages/connectpage/forkme.js
+++ client/pages/connectpage/forkme.js
@@ -1,5 +1,3 @@
-/* eslint-disable react/jsx-no-duplicate-props */
-/* eslint-disable react/jsx-no-target-blank */
 import React from "react";
 import "./forkme.scss";
 import { t } from "../../locales/";
