3e58176c86513247c13d8976197c7518d54f522d
joeshakely <joeshakely@gmail.com>
2024-07-24 11:33:30 -0700

    asdf

--- client/components/input.js
+++ client/components/input.js
@@ -1,92 +1,86 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import { nop } from "../helpers/";
 import "./input.scss";
 
-export class Input extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+export const Input = ({ type, onMouseDown, onMouseUp, onChange, ...props }) => {
+    const inputRef = useRef(null);
 
-    render() {
-        switch(this.props.type) {
-        case "checkbox":
-            return (
-                <div
-                    className="component_checkbox"
-                    onMouseDown={this.props.onMouseDown && this.props.onMouseDown.bind(this)}
-                    onMouseUp={this.props.onMouseUp && this.props.onMouseUp.bind(this)}>
-                    <input
-                        type="checkbox"
-                        {...this.props}
-                        onChange={this.props.onChange || nop}
-                        ref={(comp) => this.ref = comp } />
-                    <span className="indicator"></span>
-                </div>
-            );
-        default:
-            return (
-                <input
-                    className="component_input"
-                    onChange={this.props.onChange || nop}
-                    {...this.props}
-                    ref={(comp) => this.ref = comp } />
-            );
-        }
-    }
-}
+    const handleMouseDown = onMouseDown ? onMouseDown.bind(this) : null;
+    const handleMouseUp = onMouseUp ? onMouseUp.bind(this) : null;
+
+    return type === "checkbox" ? (
+        <div
+            className="component_checkbox"
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}>
+            <input
+                type="checkbox"
+                {...props}
+                onChange={onChange || nop}
+                ref={inputRef} />
+            <span className="indicator"></span>
+        </div>
+    ) : (
+        <input
+            className="component_input"
+            onChange={onChange || nop}
+            {...props}
+            ref={inputRef} />
+    );
+};
 
 Input.propTypes = {
     type: PropTypes.string,
     placeholder: PropTypes.string,
+    onMouseDown: PropTypes.func,
+    onMouseUp: PropTypes.func,
+    onChange: PropTypes.func,
 };
 
+export const Select = ({ choices = [], id, onChange, name, value, placeholder }) => {
+    const selectId = id ? { id } : {};
 
-export const Select = (props) => {
-    const choices = props.choices || [];
-    const id = props.id ? { id: props.id } : {};
     return (
-        <select className="component_select" onChange={props.onChange} {...id}
-            name={props.name} defaultValue={props.value}>
-            <option hidden>{ props.placeholder }</option>
-            {
-                choices.map((choice, index) => {
-                    return (
-                        <option key={index} name={choice}>{choice}</option>
-                    );
-                })
-            }
+        <select className="component_select joe" onChange={onChange} {...selectId} name={name} defaultValue={value}>
+            <option hidden>{placeholder}</option>
+            {choices.map((choice, index) => (
+                <option key={index} value={choice}>{choice}</option>
+            ))}
         </select>
     );
 };
 
-export class Enabler extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+Select.propTypes = {
+    choices: PropTypes.array,
+    id: PropTypes.string,
+    onChange: PropTypes.func,
+    name: PropTypes.string,
+    value: PropTypes.string,
+    placeholder: PropTypes.string,
+};
 
-    componentDidMount() {
+export const Enabler = ({ target = [], defaultValue, onChange }) => {
+    const inputRef = useRef(null);
+
+    useEffect(() => {
         requestAnimationFrame(() => {
-            this.toggle(this.props.defaultValue || false);
+            toggle(defaultValue || false);
         });
-    }
+    }, [defaultValue]);
 
-    onChange(e) {
-        this.toggle(e.target.checked);
-        this.props.onChange(e);
-    }
-
-    toggle(value) {
-        const target = this.props.target || [];
-        target.map((t) => {
+    const toggle = (value) => {
+        target.forEach((t) => {
             const $el = document.getElementById(t);
             if (!$el) return;
+
+            const parentElement = $el.parentElement.parentElement.parentElement.parentElement;
             if (value === true) {
-                $el.parentElement.parentElement.parentElement.parentElement.style.display = "block";
-                $el.parentElement.parentElement.parentElement.parentElement.style.opacity = "1";
+                parentElement.style.display = "block";
+                parentElement.style.opacity = "1";
             } else {
-                $el.parentElement.parentElement.parentElement.parentElement.style.display = "none";
-                $el.parentElement.parentElement.parentElement.parentElement.style.opacity = "0";
+                parentElement.style.display = "none";
+                parentElement.style.opacity = "0";
 
                 // reset value
                 if ($el.value) {
@@ -97,12 +91,22 @@ export class Enabler extends React.Component {
                 }
             }
         });
-    }
+    };
 
-    render() {
-        return (
-            <Input type="checkbox" onChange={this.onChange.bind(this)}
-                defaultChecked={this.props.defaultValue} />
-        );
-    }
-}
+    const handleChange = (e) => {
+        toggle(e.target.checked);
+        if (onChange) {
+            onChange(e);
+        }
+    };
+
+    return (
+        <Input type="checkbox" onChange={handleChange} defaultChecked={defaultValue} ref={inputRef} />
+    );
+};
+
+Enabler.propTypes = {
+    target: PropTypes.array,
+    defaultValue: PropTypes.bool,
+    onChange: PropTypes.func,
+};
